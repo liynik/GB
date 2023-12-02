8b2792803b0b119ea69fb0666c9bf800eafe1940
Liliya Nik <liliy210197@gmail.com>
2023-12-02 23:52:15 +0200

    Решила три домашние задачи из семинара 3

---
+++ C sharp/практика/ДЗ_к_семинарам/Семинар_3_дз/Tasks/task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int[] GenerateArray(int size, int leftRange, int rigthRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+        {
+        tempArray[i] = rand.Next(leftRange, rigthRange + 1);
+        }
+    return tempArray;
+}
+
+void PrintArray(int[] array2){
+System.Console.WriteLine("[" + string.Join(", ", array2) + "]");
+}
+
+int size = 10;
+int count = 0;
+int[] array = GenerateArray(size, 20, 90);
+PrintArray(array);
+
+for( int i = 0; i < array.Length; i++){
+    if (array[i] >= 20 && array[i] <= 90)
+    count++;
+}
+System.Console.WriteLine($"Количество элементов массива, значения которых лежат на отрезке [20,90] {count}");
---
+++ C sharp/практика/ДЗ_к_семинарам/Семинар_3_дз/Tasks/task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 2: Задайте массив на 10 целых чисел.
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] GenerateArray(int size, int leftRange, int rigthRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+        {
+        tempArray[i] = rand.Next(leftRange, rigthRange + 1);
+        }
+    return tempArray;
+}
+
+void PrintArray(int[] array2){
+System.Console.WriteLine("[" + string.Join(", ", array2) + "]");
+}
+
+int size = 10;
+int count = 0;
+int[] array = GenerateArray(size, 1, 100);
+PrintArray(array);
+
+for( int i = 0; i < array.Length; i++){
+    if (array[i] % 2 == 0)
+    count++;
+}
+System.Console.WriteLine($"Количество чётных элементов массива: {count}");
+
