19c558fd6ffe21aae234aa18ad1504e7642c50dd
Liliya Nik <liliy210197@gmail.com>
2023-12-13 21:48:14 +0200

    Решение первой задачи из третьего семинара

---
+++ C sharp/практика/ДЗ_к_семинарам/Семинар_7_дз/task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+// M=17
+
+void ShowRealNumbers(int currentNumber, int number)
+{   
+    if (currentNumber <= number)
+    {
+        Console.Write(currentNumber + " ");
+        ShowRealNumbers(currentNumber + 1, number);
+    }
+}
+
+Console.Write("Введите число N: ");
+if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+{
+    Console.Write($"N={number} => ");
+    ShowRealNumbers(17, number);
+    Console.WriteLine();
+}
+else { Console.WriteLine("Некорректный ввод. Введите положительное целое число."); }
